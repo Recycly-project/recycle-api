8e488ec07f8f635ff36d474903e7226095966fe0
renalsugiana <a22100107@mhs.stmik-sumedang.ac.id>
2024-11-11 22:04:02 +0700

    config .env

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes/routes.js')
-
+const routes = require('./routes/routes.js');
+require('dotenv').config();
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: [ '*' ],
-            }
-        }
-    });
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    server.route(routes)
-    
-    await server.start();
-    console.log(`running on`, server.info.uri);
-}
+  server.route(routes);
+
+  await server.start();
+  console.log(`running on`, server.info.uri);
+};
 
 init();
